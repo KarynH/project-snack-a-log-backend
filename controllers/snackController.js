4d469f2b058c5dd0280f87b92a7d99d3b923579a
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2023-01-30 19:39:59 -0500

    Add create query and post to queries and snackController. Add snacks route to app.js

--- controllers/snackController.js
+++ controllers/snackController.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const snacks = express.Router();
+const { createSnack } = require("../queries/snacks");
 
 snacks.get("/", (req, res) => {
   res.json("All snacks");
+});
+
+// CREATE
+snacks.post("/", async (req, res) => {
+  try {
+    const snack = await createSnack(req.body);
+    res.json(snack);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 module.exports = snacks;
