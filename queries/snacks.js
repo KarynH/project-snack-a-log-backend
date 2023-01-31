7832c1b1d622d673fea90108a8f37c7219bab612
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2023-01-31 11:37:02 -0500

    Add capitalization function for snack names in new/create queries

--- queries/snacks.js
+++ queries/snacks.js
@@ -1,5 +1,12 @@
 const db = require("../db/dbConfig.js");
 
+// FORMAT SNACK NAME
+function formatString(string) {
+  return string.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+    letter.toUpperCase()
+  );
+}
+
 // SHOW ALL SNACKS
 const getAllSnacks = async () => {
   try {
@@ -17,7 +24,7 @@ const createSnack = async (snack) => {
   try {
     const newSnack = await db.one(
       "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-      [name, fiber, protein, added_sugar, is_healthy, image]
+      [formatString(name), fiber, protein, added_sugar, is_healthy, image]
     );
     return newSnack;
   } catch (error) {
@@ -31,7 +38,7 @@ const updateSnack = async (id, snack) => {
   try {
     const updatedSnack = await db.one(
       "UPDATE snacks SET name=$1, fiber=$2, protein=$3, added_sugar=$4, is_healthy=$5, image=$6 WHERE id=$7 RETURNING *",
-      [name, fiber, protein, added_sugar, is_healthy, image, id]
+      [formatString(name), fiber, protein, added_sugar, is_healthy, image, id]
     );
     return updatedSnack;
   } catch (error) {
