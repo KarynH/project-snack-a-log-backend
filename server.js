f2f6b176968b03c5ceb53a4699f59d8153f744fa
i-guerrero <isaacguerrero@pursuit.org>
2023-02-01 19:31:41 -0500

    Deploy backend via fly.io; changed app.listen in server.js

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ require("dotenv").config();
 // const PORT = process.env.PORT;
 
 // LISTEN
+// changed to fix errors when deploying with fly.io
 app.listen({ port: process.env.PORT, host: "0.0.0.0" });
